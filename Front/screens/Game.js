1665fe896191763f772f446bc9e8f15a5b956e7d
Jules <jules.magyari@epitech.eu>
2023-03-16 12:46:58 +0100

    [ADD] add 2 new screens

--- Front/screens/Game.js
+++ Front/screens/Game.js
@@ -1,10 +1,137 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+
+import {Image, View, Text, StyleSheet, ScrollView} from 'react-native';
+
+import {InnerContainer} from './../components/styles';
 
 export default function Game({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Game Screen</Text>
-        </View>
+        <InnerContainer>
+            <ScrollView marginTop={10} showsVerticalScrollIndicator={false} directionalLockEnabled={true}>
+                <View style={styles.categoryContainer} marginTop={50}>
+                    <Text style={styles.categoryTitle}>Joué récemment</Text>
+                </View>
+                <ScrollView horizontal={true} style={styles.scrollGame} showsHorizontalScrollIndicator={false}>
+                    <Image source={require('./../assets/img/games/satisfactory.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/slaythespire.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/simcity.png')} style={styles.games}/>
+                </ScrollView>
+                <View style={styles.categoryContainer}>
+                    <Text style={styles.categoryTitle}>Complétions</Text>
+                </View>
+                <ScrollView horizontal={true} style={styles.scrollGame} showsHorizontalScrollIndicator={false}>
+                    <View style={styles.gameStyle}>
+                        <Image source={require('./../assets/img/games/zeroranger.png')} style={styles.games}/>
+                        <View style={styles.opacity}/>
+                        <View style={styles.percentage}>
+                            <Text style={styles.infos}>100%</Text>
+                            <View style={{backgroundColor: "#FFE500", height: 5, width: 100, marginTop: 2, borderRadius: 5}}/>
+                        </View>
+                    </View>
+                    
+                    <View style={styles.gameStyle}>
+                        <Image source={require('./../assets/img/games/ageofmythology.png')} style={styles.games}/>
+                        <View style={styles.opacity}/>
+                        <View style={styles.percentage}>
+                            <Text style={styles.infos}>84%</Text>
+                            <View style={{backgroundColor: "#FFE500", height: 5, width: 84, marginTop: 2, borderRadius: 5}}/>
+                        </View>
+                    </View>
+                    <View style={styles.gameStyle}>
+                        <Image source={require('./../assets/img/games/personaroyal.png')} style={styles.games}/>
+                        <View style={styles.opacity}/>
+                        <View style={styles.percentage}>
+                            <Text style={styles.infos}>62%</Text>
+                            <View style={{backgroundColor: "#FFE500", height: 5, width: 62, marginTop: 2, borderRadius: 5}}/>
+                        </View>
+                    </View>
+                    <View style={{height: 173}}></View>
+                </ScrollView>
+                <View style={styles.categoryContainer}>
+                    <Text style={styles.categoryTitle}>Vos prochaines sorties</Text>
+                </View>
+                <ScrollView horizontal={true} style={styles.scrollGame} showsHorizontalScrollIndicator={false}>
+                    <View style={styles.gameStyle}>
+                        <Image source={require('./../assets/img/games/hades2.png')} style={styles.games}/>
+                        <View style={styles.opacity}/>
+                        <View style={styles.percentage}>
+                            <Text style={styles.infos}>75</Text>
+                            <Text style={styles.days}>Jours</Text>
+                        </View>
+                    </View>
+                    <View style={styles.gameStyle}>
+                        <Image source={require('./../assets/img/games/hollowknight2.png')} style={styles.games}/>
+                        <View style={styles.opacity}/>
+                        <View style={styles.percentage}>
+                            <Text style={styles.infos}>203</Text>
+                            <Text style={styles.days}>Jours</Text>
+                        </View>
+                    </View>
+                    <View style={styles.gameStyle}>
+                        <Image source={require('./../assets/img/games/earthblade.png')} style={styles.games}/>
+                        <View style={styles.opacity}/>
+                        <View style={styles.percentage}>
+                            <Text style={styles.infos}>2</Text>
+                            <Text style={styles.days}>Ans</Text>
+                        </View>
+                    </View>
+                    <View style={{height: 173}}></View>
+                </ScrollView>
+                <View style = {{backgroundColor: "#121212", height: 120}}/>
+            </ScrollView>
+        </InnerContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    categoryContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 15,
+        justifyContent: 'space-between',
+    },
+    categoryTitle: {
+        color: "#FFF",
+        fontSize: 30,
+    },
+    showAll: {
+        color: "#FFE500",
+        marginRight: 10,
+        marginTop: 10,
+    },
+    scrollGame: {
+        marginTop: 10,
+        marginRight: 10,
+    },
+    games: {
+        width: 130,
+        height: 173,
+        resizeMode: 'contain',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    opacity: {
+        backgroundColor: "#000",
+        width: 130,
+        height: 173,
+        marginTop: -173,
+        opacity: 0.5,
+        borderRadius: 10,
+    },
+    gameStyle: {
+        alignItems: 'center',
+    },
+    infos: {
+        color: "#FFE500",
+        fontSize: 35,
+        fontWeight: 'bold',
+    },
+    percentage: {
+        alignItems: 'center',
+        marginTop: -140
+    },
+    days: {
+        color: "#FFE500",
+        fontSize: 25,
+    },
+})
--- Front/screens/Search.js
+++ Front/screens/Search.js
@@ -1,10 +1,101 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView, Image } from 'react-native';
+
+import {
+    InnerContainer,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    StyledButton,
+    ButtonText,
+    Line,
+    Avatar,
+    StyledContainer,
+} from './../components/styles';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
 
 export default function Search({ navigation }) {
+    const [text, onChangeText] = React.useState(false);
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 26, fontWeight: 'bold' }}>Search Screen</Text>
-        </View>
+        <InnerContainer>
+            <View>
+                <TextInput style={styles.input} onChangeText={onChangeText} value={text} placeholder="Recherche"/>
+                <Ionicons name={'search'} color={"#D9D9D9"} size={20} style={{marginTop: -25, marginLeft: 10}}/>
+            </View>
+            <ScrollView marginTop={10} showsVerticalScrollIndicator={false} directionalLockEnabled={true}>
+                <View style={styles.categoryContainer} marginTop={30}>
+                    <Text style={styles.categoryTitle}>Jeux en tendances</Text>
+                </View>
+                <ScrollView horizontal={true} style={styles.scrollGame} showsHorizontalScrollIndicator={false}>
+                    <Image source={require('./../assets/img/games/pizzatower.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/thymesia.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/cursedgolf.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/bearbreakfast.png')} style={styles.games}/>
+                </ScrollView>
+                <View style={styles.categoryContainer} marginTop={30}>
+                    <Text style={styles.categoryTitle}>Vos amis ont aimé</Text>
+                </View>
+                <ScrollView horizontal={true} style={styles.scrollGame} showsHorizontalScrollIndicator={false}>
+                    <Image source={require('./../assets/img/games/castlevania.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/eldenring.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/entergungeon.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/noita.png')} style={styles.games}/>
+                </ScrollView>
+                <View style={styles.categoryContainer} marginTop={30}>
+                    <Text style={styles.categoryTitle}>Sortie anticipées</Text>
+                </View>
+                <ScrollView horizontal={true} style={styles.scrollGame} showsHorizontalScrollIndicator={false}>
+                    <Image source={require('./../assets/img/games/scorn.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/godofwar.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/hogwarts.png')} style={styles.games}/>
+                    <Image source={require('./../assets/img/games/zelda.png')} style={styles.games}/>
+                </ScrollView>
+                <View style = {{backgroundColor: "#121212", height: 120}}/>
+            </ScrollView>
+        </InnerContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        marginTop: 40,
+        height: 30,
+        width: 380,
+        borderWidth: 1,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 30,
+        backgroundColor: "#313131",
+        color: "#D9D9D9",
+        borderRadius: 10,
+    },
+    categoryContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 15,
+        justifyContent: 'space-between',
+    },
+    categoryTitle: {
+        color: "#FFF",
+        fontSize: 30,
+    },
+    showAll: {
+        color: "#FFE500",
+        marginRight: 10,
+        marginTop: 10,
+    },
+    scrollGame: {
+        marginTop: 10,
+        marginRight: 10,
+    },
+    games: {
+        width: 130,
+        height: 173,
+        resizeMode: 'contain',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+});
